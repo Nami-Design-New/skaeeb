d2d0272e34e8d593656ec17140eccc5e3abb5239
mahmoudelkomy996 <94573066+namisoftgroup@users.noreply.github.com>
2023-04-14 11:34:06 +0200

    about

--- js/custom.js
+++ js/custom.js
@@ -84,4 +84,21 @@ $(document).ready(function () {
   const tooltipList = [...tooltipTriggerList].map(
     (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
   );
+  // counter up
+  const counterUp = window.counterUp.default
+  const callback = entries => {
+    entries.forEach(entry => {
+      const el = entry.target
+      if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+        counterUp(el, {
+          duration: 3000,
+          delay: 16,
+        })
+        el.classList.add('is-visible')
+      }
+    })
+  }
+  const IO = new IntersectionObserver(callback, { threshold: 1 })
+  const elements = document.querySelectorAll('.counterUp')
+  elements.forEach(el => IO.observe(el))
 });
