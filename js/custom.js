2baf210ee616cf03066bb4331599ad007fd4a665
Mahmoud Elkomy <82784354+mahmoudelkomy996@users.noreply.github.com>
2023-05-29 14:30:02 +0300

    end

--- js/custom.js
+++ js/custom.js
@@ -44,6 +44,27 @@ $(document).ready(function () {
       disableOnInteraction: false,
     },
   });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+  
 });
 // ////////////////////////////////////////
 // ////////////////////////////////////////
